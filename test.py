0b13ccca09db9c24a46ede3f35e8f47d06199c6b
Jael Gu <mengjia.gu@zilliz.com>
2021-12-16 12:23:09 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,24 @@
+import os
 from numpy import linalg as LA
 from towhee import pipeline
 from PIL import Image
 
-embedding_pipeline = pipeline('image-embedding')
+encoder = pipeline('image-embedding')
 
-img = Image.open(img_path)
-feat = embedding_pipeline(img)
-norm_feat = feat / LA.norm(feat)
+def extract_features(img_dir):
+    vectors = []
+    pics = []
+    for x in os.listdir(img_dir):
+        img_path = os.path.join(img_dir, x)
+        img = Image.open(img_path)
+        feat = encoder(img)
+        norm_feat = feat / LA.norm(feat)
+        vectors.append(norm_feat.tolist()[0][0])
+        pics.append(img_path)
+    return vectors, pics
+
+vectors, names = extract_features(image_dir)
+ids = milvus_client.insert(table_name, vectors)
+milvus_client.create_index(table_name)
+mysql_cli.create_mysql_table(table_name)
+mysql_cli.load_data_to_mysql(table_name, format_data(ids, names))
